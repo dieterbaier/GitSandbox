538c2e1488435c1a8106d8c614788ad71758c2d5
dbaier <dieter.baier@novatec-gmbh.de>
2013-07-05 06:37:29 +0200

    Initial commit

---
+++ src/dieterbaier/git/sandbox/Greeter.java
@@ -0,0 +1,9 @@
+package dieterbaier.git.sandbox;
+
+public class Greeter {
+
+  public String sayHello(final String name) {
+    return new StringBuilder("Hello").append(" ").append(name).toString();
+  }
+
+}
---
+++ src/dieterbaier/git/sandbox/Main.java
@@ -0,0 +1,12 @@
+package dieterbaier.git.sandbox;
+
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(final String[] args) {
+    System.out.println(new Greeter().sayHello("Dieter"));
+  }
+
+}
